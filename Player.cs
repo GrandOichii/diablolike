82a64459472138a35bd4bd1815583e09e401feb3
Igor Yastrebov <i.hawk@yandex.ru>
2023-07-26 00:22:55 +0400

    Added idle and run animations

--- Player.cs
+++ Player.cs
@@ -9,7 +9,9 @@ public partial class Player : CharacterBody3D
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
 	
-	private Node3D Mesh;
+	private Node3D MeshNode;
+	private Camera3D CameraNode;
+	private AnimationTree AnimationsNode;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
@@ -18,7 +20,41 @@ public partial class Player : CharacterBody3D
 	private Vector3 MoveTransform = new(1, 1, 1);
 	
 	public override void _Ready() {
-		Mesh = GetNode<Node3D>("%Mesh");
+		MeshNode = GetNode<Node3D>("%Mesh");
+		CameraNode = GetNode<Camera3D>("%Camera");
+		AnimationsNode = GetNode<AnimationTree>("%Animations");
+	}
+	
+	private bool _fullscreen = false;
+	public override void _Input(InputEvent e) {
+		if (e.IsActionPressed("zoom_in")) {
+			HandleZoom(4);
+			
+		}
+		if (e.IsActionPressed("zoom_out")) {
+			HandleZoom(-4);
+		}
+		if (e.IsActionPressed("toggle_fullscreen")) {
+			var flag = DisplayServer.WindowMode.Fullscreen;
+			if (_fullscreen) flag = DisplayServer.WindowMode.Windowed;
+			DisplayServer.WindowSetMode(flag);
+			_fullscreen = !_fullscreen;
+		}
+		
+	}
+	
+	private readonly float _zoomYMin = 4;
+	private readonly float _zoomYMax = 12;
+	private void HandleZoom(float v) {
+		var d = CameraNode.Position.DirectionTo(MeshNode.Position);
+		var target = CameraNode.Position + (-d * v);
+		if (target.Y > _zoomYMax || target.Y < _zoomYMin) return;
+		
+		GD.Print(target);
+		CreateTween()
+			.TweenProperty(CameraNode, "position", target, .3f)
+			.SetTrans(Tween.TransitionType.Quad);
+//		GD.Print();
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -39,30 +75,13 @@ public partial class Player : CharacterBody3D
 		Vector3 v = new Vector3((inputDir.Y + inputDir.X) / 2, 0, inputDir.X - inputDir.Y);
 //		Vector3 v = new Vector3(inputDir.Y - inputDir.X, 0, (inputDir.Y + inputDir.X*2) / 2);
 
-		var mt = MoveTransform * v;
 		Vector3 direction = (MoveTransform * v).Normalized();
 
 		if (direction != Vector3.Zero)
 		{
 			var target = MathF.Atan2(Velocity.X,Velocity.Z);
-//			var diff = Mesh.Rotation.AngleTo(new(Mesh.Rotation.X, target, Mesh.Rotation.Z));
-			var diff = Mesh.Rotation.Y - target;
-			
-			var t = (float)Math.Tau;
-			if (diff > t) {
-				GD.Print(diff);
-				diff -= t;
-			}
-			if (diff < t) {
-				GD.Print(diff);	
-				diff += t;
-			}
-//			GD.Print(diff);
-//				target -= 2 * (float)Math.PI;
-			EmitSignal(SignalName.UpdateLabel, 1, "Rotation: " + target);
-			CreateTween().TweenProperty(Mesh, "rotation", new Vector3(Rotation.X, Mesh.Rotation.Y - diff, Rotation.Z), .1f);
-//			CreateTween().TweenProperty(Mesh, "rotation", new Vector3(Rotation.X, target, Rotation.Z), .1f);
-			// Mesh.Rotation = new Vector3(Rotation.X, Mesh.Rotation.Y + diff, Rotation.Z);
+			var diff = (float)Mathf.Wrap(target - MeshNode.Rotation.Y, -Math.PI, Math.PI);
+			CreateTween().TweenProperty(MeshNode, "rotation", new Vector3(MeshNode.Rotation.X, MeshNode.Rotation.Y + diff, MeshNode.Rotation.Z), .1f);			
 			
 			velocity.X = direction.X * Speed;
 			velocity.Z = direction.Z * Speed;
@@ -74,6 +93,10 @@ public partial class Player : CharacterBody3D
 		}
 
 		Velocity = velocity;
+		var idling = inputDir == Vector2.Zero;
+		AnimationsNode.Set("parameters/conditions/idle", idling);
+		AnimationsNode.Set("parameters/conditions/run", !idling);
+		
 		MoveAndSlide();
 	}
 }

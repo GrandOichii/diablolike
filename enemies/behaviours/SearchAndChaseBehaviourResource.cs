a41958259c8a6bc6bca49edf283d041e5b77da95
Igor Yastrebov <i.hawk@yandex.ru>
2023-08-03 23:58:46 +0400

    Work on fixing enemy behaviour issues

--- enemies/behaviours/SearchAndChaseBehaviourResource.cs
+++ enemies/behaviours/SearchAndChaseBehaviourResource.cs
@@ -3,9 +3,9 @@ using System;
 
 using Godot.Collections;
 
-//public partial class IdleState : State {
-//
-//}
+public partial class IdleState : State {
+
+}
 
 
 public partial class SearchAndChaseBehaviourResource : EnemyBehaviourResource
@@ -16,35 +16,36 @@ public partial class SearchAndChaseBehaviourResource : EnemyBehaviourResource
 		Chase
 	}
 	
-//	[Export]
-//	public Dictionary<string, string> AnimationMap { get; set; } = new();
+	[Export]
+	public Dictionary<string, string> AnimationMap { get; set; } = new();
 	
-//	[Export]
-//	public NodePath AnimationsNode { get; set; }
-//
-//	private AnimationTree AnimationTreeNode { get; set; }
+	[Export]
+	public NodePath AnimationsNode { get; set; }
 
-// 	override public int CurrentState {
-//		set {
-//			base.CurrentState = value;
-////			foreach (var animKey in AnimationMap.Values)
-////				AnimationsNode.Set("parameters/conditions/" + animKey, false);
-////			AnimationsNode.Set("parameters/conditions/" + AnimationMap[(int)value], true);
-//		}
-//	}
+	private AnimationTree AnimationTreeNode { get; set; }
 
-//
-//	public SearchAndChaseBehaviourResource() : base() {
-//		CurrentState = (int)BStates.Idle;
-//
-//		States[(int)BStates.Idle] = new IdleState();
-//
-////		GD.Print(AnimationsNode);
-//	}
+ 	override public int CurrentState {
+		set {
+			base.CurrentState = value;
+//			foreach (var animKey in AnimationMap.Values)
+//				AnimationsNode.Set("parameters/conditions/" + animKey, false);
+//			AnimationsNode.Set("parameters/conditions/" + AnimationMap[(int)value], true);
+		}
+	}
+
+
+	public SearchAndChaseBehaviourResource() : base() {
+		States[(int)BStates.Idle] = new IdleState();
+		
+		CurrentState = (int)BStates.Idle;
+
+	}
 
 	public override void OnBodyDetected(Node3D body) {
+		GD.Print(AnimationsNode);
+		
 		if (body is not Player) return;
 
-//		Blackboard.Add("player", body as Player);
+		Blackboard.Add("player", body as Player);
 	}
 }
--- enemies/resources/EnemyBehaviourResource.cs
+++ enemies/resources/EnemyBehaviourResource.cs
@@ -3,33 +3,32 @@ using System;
 
 using Godot.Collections;
 
-//public partial class State : Node {
-//	public virtual void Start() {}
-//	public virtual void Process(EnemyBase controlled, double delta) {}
-//	public virtual void Stop() {}
-//}
+public partial class State : Node {
+	public virtual void Start() {}
+	public virtual void Process(EnemyBase controlled, double delta) {}
+	public virtual void Stop() {}
+}
 
-//[Tool]
 public partial class EnemyBehaviourResource : Resource
 {
-//	private int _currentState = 0;
-//	virtual public int CurrentState {
-//		get => _currentState;
-//		set {
-////			States[_currentState].Stop();
-//			_currentState = value;
-////			States[_currentState].Start();
-//		}
-//	}
-//	public Dictionary<int, State> States { get; set; } = new();
-//	public Dictionary<string, Variant> Blackboard = new();
+	private int _currentState = 0;
+	virtual public int CurrentState {
+		get => _currentState;
+		set {
+			States[_currentState].Stop();
+			_currentState = value;
+			States[_currentState].Start();
+		}
+	}
+	public Dictionary<int, State> States { get; set; } = new();
+	public Dictionary<string, Variant> Blackboard = new();
 //
 	public void Process(EnemyBase controlled, double delta) {
-//		States[CurrentState].Process(controlled, delta);
+		States[CurrentState].Process(controlled, delta);
 	}
 //
 	public virtual void OnBodyDetected(Node3D body) {
 	}
 //
-//	public bool HasValue(string key) => Blackboard.ContainsKey(key);
+	public bool HasValue(string key) => Blackboard.ContainsKey(key);
 }
